2520aaad6c16dd2aae6d45e69e0d875cc69c5af3
Dovydev <info@leakoni.net>
2019-11-09 00:58:09 +0100

    test

--- commands/pat.js
+++ commands/pat.js
@@ -9,24 +9,13 @@ exports.run = async (client, message, args) => {
     if (!perms.has('SEND_MESSAGES')) return noBotPerms(message, 'SEND_MESSAGES');
     if (!perms.has('VIEW_CHANNEL')) return noBotPerms(message, 'VIEW_CHANNEL');
 
-    var links = undefined;
-    var link = "";
-
-    fs.readFile('../akane/actions/pat.txt', function(err, data)
-    {
-        if (err) throw err;
-        links = data.toString().split("\n");
-        link = lines[Math.floor(Math.random()*lines.length)];
-    });
-
-
     const patEmbed = new RichEmbed()
     .setAuthor(`${message.member.user.tag} pats ${args[0]}`)
     .setImage(link);
     .setTimestamp()
     .setColor(embedColor);
 
-    message.channel.send(patEmbed);
+    await message.channel.send(patEmbed);
 };
 
 exports.help = {
